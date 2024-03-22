2b4d65ed7f3cdaff7557f4c050ed0a99c01c9fe1
Gerald Rich <newsroomdev@users.noreply.github.com>
2024-03-22 16:15:25 -0700

    es module

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,32 @@
-const fs = require("fs");
-const path = require("path");
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { resolve, join, dirname } from "path";
 
-async function scrapeData() {
-  const fetch = (await import("node-fetch")).default;
+import fetch from "node-fetch";
+import { csvFormat } from "d3-dsv";
+
+function writeData(data) {
   // Get the current timestamp
-  const timestamp = new Date().toISOString().replace(/[-:]/g, "").slice(0, 8);
+  const timestamp = new Date().toISOString();
 
   // Name the files after the timestamp
   const filenameJson = `${timestamp}.json`;
   const filenameCsv = `${timestamp}.csv`;
 
   // Define the paths
-  const baseDir = path.resolve(__dirname, "..", "..");
-  const jsonPath = path.join(baseDir, "data", "json", filenameJson);
-  const csvPath = path.join(baseDir, "data", "csv", filenameCsv);
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+  const baseDir = resolve(__dirname, "..", "..");
+  const jsonPath = join(baseDir, "data", "json", filenameJson);
+  const csvPath = join(baseDir, "data", "csv", filenameCsv);
 
+  // Write the JSON & CSV files
+  fs.writeFileSync(jsonPath, JSON.stringify(data));
+  const csv = csvFormat(data);
+  fs.writeFileSync(csvPath, csv);
+}
+
+async function scrapeData() {
   // Grab the data from the API
   const response = await fetch(
     "https://backend-unified.mylirr.org/locations?geometry=NONE&railroad=LIRR",
@@ -25,20 +37,7 @@ async function scrapeData() {
 
   // filter and transform the data
   const data = await response.json();
-
-  // Save the data in the data/json folder
-  fs.writeFileSync(jsonPath, JSON.stringify(data));
-
-  // Save the data in the data/csv folder
-  const { AsyncParser } = await import("@json2csv/node");
-  const opts = {};
-  const transformOpts = {};
-  const asyncOpts = {};
-  const parser = new AsyncParser(opts, asyncOpts, transformOpts);
-
-  const csv = await parser.parse(data).promise();
-  fs.writeFileSync(csvPath, csv);
-
+  writeData(data);
   console.log(data);
 }
 
