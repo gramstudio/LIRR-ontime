07aa192f65d09d58bd137ae2c3d05ae51c736533
Gerald Rich <newsroomdev@users.noreply.github.com>
2024-03-25 21:38:05 -0700

    ts module

---
+++ src/ts/lirr.ts
@@ -0,0 +1,91 @@
+export default interface Train {
+  train_id: string;
+  railroad: string;
+  run_date: string;
+  train_num: string;
+  realtime: boolean;
+  details: Details;
+  consist: Consist;
+  location: Location;
+  status: Status;
+  alerts?: Alert[];
+}
+
+interface Alert {
+  status: string;
+  header: string;
+  text: string;
+  priority: number;
+  start_time: number;
+  human_duration: string;
+  status_level: number;
+  station_alternatives: any[];
+  language: string;
+}
+
+interface Status {
+  otp: number;
+  otp_location: string;
+  held: boolean;
+  canceled: boolean;
+}
+
+interface Location {
+  longitude: number;
+  latitude: number;
+  nearby: string;
+  speed?: number;
+  heading?: number;
+  source: string;
+  timestamp: number;
+  extra_info?: string;
+}
+
+interface Consist {
+  fleet?: string;
+  cars: Car[];
+  actual_len?: number;
+  sched_len: number;
+  occupancy: string;
+  occupancy_timestamp?: number;
+}
+
+interface Car {
+  type: string;
+  number: number;
+  loading: string;
+  passengers: number;
+  restroom: boolean;
+  revenue: boolean;
+  bikes: number;
+  locomotive: boolean;
+  quiet?: boolean;
+}
+
+interface Details {
+  headsign: string;
+  summary: string;
+  peak_code: string;
+  branch?: string;
+  stops: Stop[];
+  events: any[];
+  direction: string;
+  bike_rule: string;
+}
+
+interface Stop {
+  code: string;
+  sched_time: number;
+  act_depart_time?: number;
+  act_time?: number;
+  sign_track: string;
+  avps_track_id: string;
+  posted: boolean;
+  t2s_track: string;
+  stop_status: string;
+  stop_type: string;
+  local_cancel: boolean;
+  bus: boolean;
+  occupancy: string;
+  track_change?: boolean;
+}
